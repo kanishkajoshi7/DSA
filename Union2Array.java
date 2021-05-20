85ef1ff149fccb09b81be75311464db64d30f552
kanishka <61345781+kanishkajoshi7@users.noreply.github.com>
2021-05-20 17:36:56 +0530

    Create Union2Array.java
    https://practice.geeksforgeeks.org/problems/union-of-two-arrays3538/1#

---
+++ Union2Array.java
@@ -0,0 +1,15 @@
+class Solution{
+    public static int doUnion(int a[], int n, int b[], int m) 
+    {
+        //Your code here
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0;i<n;i++){
+            hs.add(a[i]);
+        }
+        for(int i=0;i<m;i++){
+            hs.add(b[i]);
+        }
+        return hs.size();
+        //System.out.println(hs.size());
+    }
+}
