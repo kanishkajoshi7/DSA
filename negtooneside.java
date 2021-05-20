48aaabe958c93b0d9f9d239ac01258ed9e2388af
kanishka <61345781+kanishkajoshi7@users.noreply.github.com>
2021-05-20 15:16:00 +0530

    Update negtooneside.java

--- negtooneside.java
+++ negtooneside.java
@@ -23,3 +23,40 @@ class GFG {
             System.out.print(arr[i] + " ");
 	}
 }
+//........................................................................OR.................................................................................
+
+
+import java.io.*;
+
+class GFG {
+    static void swap(int[] arr){
+     int left=0;
+     int right=arr.length -1;
+     while(left<=right){
+         if(arr[left]<0 && arr[right]<0){
+             left++;
+         }else if(arr[left]>0 && arr[right]<0){
+             int temp=arr[left];
+             arr[left]=arr[right];
+             arr[right]=temp;
+             left++;
+             right--;
+         }else if(arr[left]>0 && arr[right]>0){
+             right--;
+         }else{
+             left++;
+             right--;
+         }
+     }
+    }
+	public static void main (String[] args) {
+	     //int arr[] = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
+	     int[] arr = { -12, 11, -13, -5,
+                   6, -7, 5, -3, 11 };
+              
+         swap(arr);
+         int n = arr.length;
+         for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+	}
+}
